277d90ff03460998540255964882e6161e059b63
Jasir Ullah Khan <55176122+jasirullah12345@users.noreply.github.com>
2021-11-04 09:46:28 +0500

    useLocation in Navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from "./components/About";
 function App() {
     return (
         <>
-            <Navbar/>
+            <Navbar text="iNotebook"/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-const Navbar = () => {
+import {Link, useLocation} from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Navbar = (props) => {
+    let location = useLocation().pathname;
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">iNotebook</Link>
+                    <Link className="navbar-brand" to="/">{props.text}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -14,15 +17,18 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                <Link className={`nav-link ${location === "/" ? "active" : ""}`} aria-current="page"
+                                      to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/about">About</Link>
+                                <Link className={`nav-link ${location === "/about" ? "active" : ""}`}
+                                      to="/about">About</Link>
                             </li>
                         </ul>
                         <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            <input className="form-control me-2" type="search" placeholder="Search"
+                                   aria-label="Search"/>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
@@ -30,5 +36,10 @@ const Navbar = () => {
         </>
     );
 };
-
+Navbar.propTypes = {
+    text: PropTypes.string
+}
+Navbar.defaultProps = {
+    text: 'Navbar'
+}
 export default Navbar;
